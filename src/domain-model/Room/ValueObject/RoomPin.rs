65f80f7b7eb3a39d1c1e95f4f735aeb3c6743a6f
NobuyaTagawa <090.sket@gmail.com>
2024-05-05 00:20:18 +0900

    fix: コンストラクタ修正

--- src/domain-model/Room/ValueObject/RoomPin.rs
+++ src/domain-model/Room/ValueObject/RoomPin.rs
@@ -6,9 +6,9 @@ pub struct RoomPin {
 
 /// 000000から999999までの乱数を生成
 impl RoomPin {
-  pub fn new() -> RoomPin {
+  pub fn new() -> Self{
     RoomPin {
-      rand::thread_rng().gen_range(000000,1000000)
+      rand.thread_rng().gen_range(000000,1000000)
     }
   }
 }
