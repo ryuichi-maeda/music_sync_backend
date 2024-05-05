5ee402021454f94585aeeac5eba45cb552f40f2e
Tagawa Nobuya <107915399+TenyaVcore@users.noreply.github.com>
2024-05-05 17:10:12 +0900

    Revert "Revert "Developをmainにマージ""

---
+++ src/domain-model/User/User.rs
@@ -0,0 +1,21 @@
+
+pub trait User {
+  fn Id(&self) -> UserID {
+    self.id
+  }
+
+  fn name(&self) -> UserName {
+    self.user_name.clone()
+  }
+
+  fn user_type(&self) -> UserType {
+    self.user_type
+  }
+
+  fn is_deleted(&self) -> bool {
+    self.is_deleted
+  }
+
+
+}
+
