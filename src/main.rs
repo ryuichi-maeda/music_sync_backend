c86684282b566b70a59e7294c569ced326435904
ryuichi-maeda <uryu44213@gmail.com>
2024-06-02 00:04:39 +0900

    feat: サンプルクエリ追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::{
+    http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema, SimpleObject,
+};
+use async_graphql_axum::GraphQL;
+use axum::{
+    response::{self, IntoResponse},
+    routing::get,
+    Router,
+};
+use tokio::net::TcpListener;
+
+#[derive(SimpleObject, sqlx::FromRow)]
+struct Ping {
+    status: String,
+    code: i32,
+}
+
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn ping(&self) -> Ping {
+        Ping {
+            status: "ok".to_string(),
+            code: 200,
+        }
+    }
+}
+
+async fn graphiql() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+
+    let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
+
+    println!("GraphiQL IDE: http://localhost:8000");
+
+    axum::serve(TcpListener::bind("0.0.0.0:8000").await.unwrap(), app)
+        .await
+        .unwrap();
 }
