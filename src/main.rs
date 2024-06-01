a815e96dd28223c7b52ba481600fe1a87a2c9c2f
ryuichi-maeda <uryu44213@gmail.com>
2024-06-02 01:42:18 +0900

    feat: サンプルコード追加

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use async_graphql::{
-    http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema, SimpleObject,
-};
-use async_graphql_axum::GraphQL;
+use async_graphql::{http::GraphiQLSource, Object, Schema, SimpleObject, Subscription};
+use async_graphql_axum::{GraphQL, GraphQLSubscription};
 use axum::{
     response::{self, IntoResponse},
     routing::get,
     Router,
 };
+use futures_util::stream::Stream;
+use std::time::Duration;
 use tokio::net::TcpListener;
 
 #[derive(SimpleObject, sqlx::FromRow)]
@@ -15,6 +15,7 @@ struct Ping {
     code: i32,
 }
 
+// Query
 struct QueryRoot;
 
 #[Object]
@@ -27,15 +28,52 @@ impl QueryRoot {
     }
 }
 
+// Mutation
+struct MutationRoot;
+
+#[Object]
+impl MutationRoot {
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+// Subscription
+struct SubscriptionRoot;
+
+#[Subscription]
+impl SubscriptionRoot {
+    async fn interval(&self, #[graphql(default = 1)] n: i32) -> impl Stream<Item = i32> {
+        let mut value = 0;
+        async_stream::stream! {
+            loop {
+                futures_timer::Delay::new(Duration::from_secs(1)).await;
+                value += n;
+                yield value;
+            }
+        }
+    }
+}
+
 async fn graphiql() -> impl IntoResponse {
-    response::Html(GraphiQLSource::build().endpoint("/").finish())
+    response::Html(
+        GraphiQLSource::build()
+            .endpoint("/")
+            .subscription_endpoint("/ws")
+            .finish(),
+    )
 }
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot).finish();
 
-    let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
+    let app = Router::new()
+        .route(
+            "/",
+            get(graphiql).post_service(GraphQL::new(schema.clone())),
+        )
+        .route_service("/ws", GraphQLSubscription::new(schema));
 
     println!("GraphiQL IDE: http://localhost:8000");
 
