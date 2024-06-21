535ddd6edeca0568595788f232f2e541041065bf
NobuyaTagawa <090.sket@gmail.com>
2024-06-21 17:11:22 +0900

    feat: モジュール分割に必要なファイルを追加

---
+++ src/domain_service.rs
@@ -0,0 +1,3 @@
+pub mod music_library_repository_trait;
+pub mod room_repository_trait;
+pub mod user_repository_trait;
---
+++ src/infrastructure.rs
@@ -0,0 +1,3 @@
+pub mod music_library_repository;
+pub mod room_repository;
+pub mod user_repository;
