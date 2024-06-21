48db0fa514ca7ce60e0ac0bb45af2aa6a0f0b2b3
NobuyaTagawa <090.sket@gmail.com>
2024-06-21 17:16:25 +0900

    feat: find_by_idを実装

--- src/infrastructure/music_library_repository.rs
+++ src/infrastructure/music_library_repository.rs
@@ -1,65 +1,53 @@
-mod MusicLibraryRepositoryTrait;
+use crate::domain_model::music_library;
+use crate::domain_model::music_library::music_library::MusicLibrary;
+use crate::domain_model::music_library::value_object::music::Music;
+use crate::domain_model::user::value_object::user_id::UserID;
+use crate::domain_service::music_library_repository_trait::MusicLibraryRepositoryTrait;
+use anyhow::Result;
+use async_graphql::async_trait::async_trait;
+use sqlx::MySqlPool;
 
-use MusicLibraryRepositoryTrait::MusicLibraryRepositoryTrait;
-
-pub struct MusicLibraryRepository;
+pub struct MusicLibraryRepository {
+    pool: MySqlPool
+}
 
 impl MusicLibraryRepository {
-    pub fn new() -> Self {
-        MusicRepository
+    pub fn new(pool: MySqlPool) -> Self {
+        Self { pool }
     }
 }
 
-// TODO: 実装する
+#[async_trait]
 impl MusicLibraryRepositoryTrait for MusicLibraryRepository {
-    fn find_by_id(&self, id: MusicLibraryID) -> Result<MusicLibrary, MusicLibraryRepositoryError> {
+    async fn find_by_id(&self, user_id: String) -> Result<MusicLibrary> {
+        let music = sqlx::query_as!(
+            Music,
+            r#"SELECT title, artist, apple_music_id FROM music_library WHERE user_id = ?"#,
+            user_id
+        )
+        .fetch_one(&self.pool)
+        .await?;
+
         Ok(MusicLibrary {
-            id: id,
             user_id: UserID::new(),
-            music_name: MusicName::new(),
-            artist_name: ArtistName::new(),
-            genre: Genre::new(),
-            release_date: ReleaseDate::new(),
-            created_at: CreatedAt::new(),
-            updated_at: UpdatedAt::new(),
-            is_deleted: false,
+            musics: vec![music],
         })
     }
 
-    fn find_by_user_id(&self, user_id: UserID) -> Result<Vec<MusicLibrary>, MusicLibraryRepositoryError> {
-        Ok(vec![MusicLibrary {
-            id: MusicLibraryID::new(),
-            user_id: user_id,
-            music_name: MusicName::new(),
-            artist_name: ArtistName::new(),
-            genre: Genre::new(),
-            release_date: ReleaseDate::new(),
-            created_at: CreatedAt::new(),
-            updated_at: UpdatedAt::new(),
-            is_deleted: false,
-        }])
+    async fn find_by_user_id(&self, user_id: UserID) -> Result<Vec<MusicLibrary>> {
+        todo!()
     }
 
-    fn find_all(&self) -> Result<Vec<MusicLibrary>, MusicLibraryRepositoryError> {
-        Ok(vec![MusicLibrary {
-            id: MusicLibraryID::new(),
-            user_id: UserID::new(),
-            music_name: MusicName::new(),
-            artist_name: ArtistName::new(),
-            genre: Genre::new(),
-            release_date: ReleaseDate::new(),
-            created_at: CreatedAt::new(),
-            updated_at: UpdatedAt::new(),
-            is_deleted: false,
-        }])
+    async fn find_all(&self) -> Result<Vec<MusicLibrary>> {
+        todo!()
     }
 
-    fn save(&self, music_library: MusicLibrary) -> Result<(), MusicLibraryRepositoryError> {
-        Ok(())
+    async fn save(&self, music_library: MusicLibrary) -> Result<()> {
+        todo!()
     }
 
-    fn delete(&self, music_library: MusicLibrary) -> Result<(), MusicLibraryRepositoryError> {
-        Ok(())
+    async fn delete(&self, music_library: MusicLibrary) -> Result<()> {
+        todo!()
     }
 }
 
