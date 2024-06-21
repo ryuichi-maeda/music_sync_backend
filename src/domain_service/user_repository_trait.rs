94d9412a8e8f58109a5f998fb2535fb43caaadcd
NobuyaTagawa <090.sket@gmail.com>
2024-06-21 17:12:48 +0900

    refactor: ファイル名をrustの命名規則に合わせ、asyncに対応させた

---
+++ src/domain_service/user_repository_trait.rs
@@ -0,0 +1,15 @@
+use crate::domain_model::user::guest_user::GuestUser;
+use crate::domain_model::user::value_object::user_id::UserID;
+
+use axum::async_trait;
+use anyhow::Result;
+
+#[async_trait]
+pub trait UserRepositoryTrait: Send + Sync {
+    async fn find_by_id(&self, id: UserID) -> Result<GuestUser>;
+    async fn find_all(&self) -> Result<Vec<GuestUser>>;
+    async fn save(&self, user: GuestUser) -> Result<()>;
+    async fn update(&self, user: GuestUser) -> Result<()>;
+    async fn delete(&self, user: GuestUser) -> Result<()>;
+    async fn check_existence(&self, id: UserID) -> Result<()>;
+}
