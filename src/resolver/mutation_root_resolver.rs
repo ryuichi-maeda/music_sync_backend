1a76ed316d559cc7b61c1fa44f3664df35692717
NobuyaTagawa <090.sket@gmail.com>
2024-06-27 17:11:02 +0900

    feat: create_music_libraryを実装

--- src/resolver/mutation_root_resolver.rs
+++ src/resolver/mutation_root_resolver.rs
@@ -1,22 +1,28 @@
 use async_graphql::{Context, Error, Object, Result};
 
-use crate::domain_model::music_library;
-use crate::domain_model::user::value_object::user_id::UserID;
+use crate::domain_model::music_library::music_library::{MusicLibrary, MusicLibraryInput};
+use crate::domain_model::user::value_object::user_id::{UserIDInput};
 use crate::Dependency;
-use crate::domain_model::music_library::music_library::MusicLibrary;
+
 // Mutation
 pub struct MutationRoot;
 
 #[Object]
 impl MutationRoot {
-    pub async fn create_music_library<'ctx>(&self, ctx: &Context<'ctx>, user_id: UserID, music_library: MusicLibrary) -> Result<MusicLibrary> {
+    pub async fn create_music_library<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        music_library: MusicLibraryInput,
+    ) -> Result<MusicLibrary> {
         let ctx = ctx.data::<Dependency>().unwrap();
-        let result = ctx.get_music_library_repository().save(user_id, music_library).await;
+        let result = ctx
+            .get_music_library_repository()
+            .save(music_library)
+            .await;
         if result.is_err() {
             return Err(Error::from(result.err().unwrap()));
         }
         let music_library = result.unwrap();
         Ok(music_library)
-        
     }
 }
