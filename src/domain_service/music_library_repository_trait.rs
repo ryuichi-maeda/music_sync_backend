a32fc3b7ae275ef24d533df1791bae223b519e8a
NobuyaTagawa <090.sket@gmail.com>
2024-06-27 17:06:48 +0900

    feat: Input用の型を追加

--- src/domain_service/music_library_repository_trait.rs
+++ src/domain_service/music_library_repository_trait.rs
@@ -1,5 +1,5 @@
-use crate::domain_model::music_library::music_library::MusicLibrary;
-use crate::domain_model::user::value_object::user_id::UserID;
+use crate::domain_model::music_library::music_library::{MusicLibrary, MusicLibraryInput};
+use crate::domain_model::user::value_object::user_id::{UserID, UserIDInput};
 use anyhow::Result;
 use axum::async_trait;
 
@@ -7,6 +7,6 @@ use axum::async_trait;
 pub trait MusicLibraryRepositoryTrait: Send + Sync {
     async fn find_by_id(&self, id: String) -> Result<MusicLibrary>;
     async fn find_by_user_id(&self, user_id: UserID) -> Result<Vec<MusicLibrary>>;
-    async fn save(&self, user_id: UserID, music_library: MusicLibrary) -> Result<MusicLibrary>;
+    async fn save(&self, music_library: MusicLibraryInput) -> Result<MusicLibrary>;
     async fn delete(&self, music_library: MusicLibrary) -> Result<()>;
 }
