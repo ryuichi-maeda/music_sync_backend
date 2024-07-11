664aba7d7fd7a033d1283740120f7ec656adfb47
NobuyaTagawa <090.sket@gmail.com>
2024-07-11 20:24:49 +0900

    fix: room_updatedのsubscriptionを追加

--- src/resolver/subscription_root_resolver.rs
+++ src/resolver/subscription_root_resolver.rs
@@ -1,13 +1,8 @@
-use async_graphql::{Context, Subscription};
+use async_graphql::{Context, Result as GqlResult, Subscription};
 use futures_util::stream::Stream;
 use std::time::Duration;
 
 use crate::domain_model::room::room::Room;
-use crate::domain_model::room::room_updated::RoomUpdated;
-use crate::domain_model::room::value_object::room_pin::RoomPin;
-use crate::domain_model::user::guest_user::GuestUser;
-use crate::domain_model::user::user::User;
-use crate::domain_model::user::value_object::user_name::UserName;
 use crate::Dependency;
 
 // Subscription
@@ -29,20 +24,21 @@ impl SubscriptionRoot {
     pub async fn room_updated<'ctx>(
         &self,
         ctx: &Context<'ctx>,
-        room_id: String,
-    ) -> impl Stream<Item = RoomUpdated> {
+        room_pin: String,
+    ) -> impl Stream<Item = GqlResult<Room>> {
         let ctx = ctx.data::<Dependency>().unwrap();
         let repository = ctx.get_room_repository();
         async_stream::stream! {
             loop {
                 futures_timer::Delay::new(Duration::from_secs(1)).await;
-                let room = repository.find_by_id(room_id.clone()).await;
-                if room.is_err() {
-                    continue;
-                } else {
-                    let room = room.unwrap();
-                    let room_updated = RoomUpdated::new(room);
-                    yield room_updated;
+                let room = repository.find_by_room_pin(room_pin.clone()).await;
+                match room {
+                    Ok(room) => {
+                        yield Ok(room);
+                    },
+                    Err(err) => {
+                        yield Err(anyhow::anyhow!(err.to_string()).into());
+                    }
                 }
             }
         }
